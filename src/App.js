9b6ea72e7d9e08276e10c7e2008102df33a50bf2
Yahia Elsayed <yahiaelsayed19@gmail.com>
2022-10-09 00:32:44 +0200

    home

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './components/home/Home';
+import Navigation from './components/navigation/Navigation';
 function App() {
   return (
     <>
+      <Navigation />
+      <Routes>
+        <Route path='/home' element={<Home />} />
+        <Route path='/about' />
+        <Route path='/gallery' />
+        <Route path='/contact' />
+      </Routes>
     </>
   );
 }
