eed11b53d06988edda0b09a3183e102c44d2c784
Yahia Elsayed <yahiaelsayed19@gmail.com>
2022-10-09 01:38:47 +0200

    portfolio

--- src/App.js
+++ src/App.js
@@ -1,17 +1,7 @@
-import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Navigation from './components/navigation/Navigation';
 function App() {
   return (
     <>
-      <Navigation />
-      <Routes>
-        <Route path='/' element={<Navigate replace to='/home' />} />
-        <Route path='/home' />
-        <Route path='/about' />
-        <Route path='/gallery' />
-        <Route path='/contact' />
-      </Routes>
     </>
   );
 }
