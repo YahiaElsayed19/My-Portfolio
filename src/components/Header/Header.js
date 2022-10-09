5872000ea2c49f01bfbef06d9464aecbcf7ce66c
Yahia Elsayed <yahiaelsayed19@gmail.com>
2022-10-09 23:40:31 +0200

    header.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navigation from './Navigation'
-import Mail from './Mail'
+import Mail from './Social'
 import classes from './Header.module.css'
 
 const Header = () => {
--- src/components/Header/Mail.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Mail = () => {
-    return (
-        <div>Mail</div>
-    )
-}
-
-export default Mail
