83b7ede568a84b9595375ba4d0aa3aa81a941fb4
Yahia Elsayed <yahiaelsayed19@gmail.com>
2022-10-10 00:00:27 +0200

    header.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
 function App() {
   return (
     <>
       <Header />
+      <Routes>
+        <Route path='/' element={<Navigate replace to='/home' />} />
+      </Routes>
     </>
   );
 }
