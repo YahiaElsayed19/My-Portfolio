97d26dbeaee2b27257e8bab14abf5ed002dc92bf
Yahia Elsayed <yahiaelsayed19@gmail.com>
2022-10-12 19:41:13 +0200

    landing

--- src/components/Home/Landing.js
+++ src/components/Home/Landing.js
@@ -1,10 +1,24 @@
-import React from 'react'
-import classes from './Landing.module.css'
+import React from "react";
+import classes from "./Landing.module.css";
 
 const Landing = () => {
     return (
-        <div>Landing</div>
-    )
-}
+        <div className={classes.landing}>
+            <div className={classes["landing-wrapper"]}>
+                <h3 className={classes["landing-h3"]}>Hey, I'm Yahia Elsayed</h3>
+                <h1 className={classes["landing-h1"]}>
+                    React Ddeveloper & React Native Develpoer
+                </h1>
+                <p className={classes["landing-p"]}>
+                    I help business grow by crafting amazing web experiences. If you're
+                    looking for a developer that likes to get stuff done, let's talk.
+                </p>
+                <a href="#my-work">
+                    <button>See my work </button>
+                </a>
+            </div>
+        </div>
+    );
+};
 
-export default Landing
+export default Landing;
