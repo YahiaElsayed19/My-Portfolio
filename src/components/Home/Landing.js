e5562af2f83a0e95cb07c44a4a199c433f8fa8b6
Yahia Elsayed <yahiaelsayed19@gmail.com>
2022-10-12 19:57:53 +0200

    landing

--- src/components/Home/Landing.js
+++ src/components/Home/Landing.js
@@ -7,14 +7,14 @@ const Landing = () => {
             <div className={classes["landing-wrapper"]}>
                 <h3 className={classes["landing-h3"]}>Hey, I'm Yahia Elsayed</h3>
                 <h1 className={classes["landing-h1"]}>
-                    React Developer & React Native Develpoer
+                    React Developer &<br /> React Native Develpoer
                 </h1>
                 <p className={classes["landing-p"]}>
                     I help business grow by crafting amazing web experiences. If you're
                     looking for a developer that likes to get stuff done, let's talk.
                 </p>
-                <a href="#my-work">
-                    <button>See my work </button>
+                <a href="#my-work" className={classes["landing-a"]}>
+                    <button className={classes["landing-btn"]}>See my work </button>
                 </a>
             </div>
 
