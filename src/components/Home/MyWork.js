bd89077fc93138251b048027f7c1f48b17f57a28
Yahia Elsayed <yahiaelsayed19@gmail.com>
2022-10-14 17:50:05 +0200

    my work

--- src/components/Home/MyWork.js
+++ src/components/Home/MyWork.js
@@ -1,85 +1,99 @@
-import React from 'react'
-import classes from './MyWork.module.css'
-import template1 from '../../assets/template1.png'
-import template2 from '../../assets/template2.png'
-import template3 from '../../assets/template3.png'
-import completeproject from '../../assets/completeproject.png'
-import quizapp from '../../assets/quizapp.png'
-import todolist from '../../assets/todolist.png'
-import expensetracker from '../../assets/expensetracker.png'
-import foodapp from '../../assets/foodapp.png'
+import React from "react";
+import classes from "./MyWork.module.css";
+import template1 from "../../assets/template1.png";
+import template2 from "../../assets/template2.png";
+import template3 from "../../assets/template3.png";
+import completeproject from "../../assets/completeproject.png";
+import quizapp from "../../assets/quizapp.png";
+import todolist from "../../assets/todolist.png";
+import expensetracker from "../../assets/expensetracker.png";
+import foodapp from "../../assets/foodapp.png";
+import { BsGithub } from 'react-icons/bs'
+import { BsFillEyeFill } from 'react-icons/bs'
+
 
 const MyWork = () => {
-    const BOX = [
+    const BOXES = [
         {
-            id: 'p1',
+            id: "p1",
+            description: "Template 1 with HTML&CSS",
             image: template1,
-            description: '',
-            github: 'https://github.com/YahiaElsayed19/Template1',
-            live: 'https://yahiaelsayed19.github.io/Template1/',
+            github: "https://github.com/YahiaElsayed19/Template1",
+            live: "https://yahiaelsayed19.github.io/Template1/",
         },
         {
-            id: 'p2',
+            id: "p2",
+            description: "Template 2 with HTML&CSS",
             image: template2,
-            description: '',
-            github: 'https://github.com/YahiaElsayed19/Template2',
-            live: 'https://yahiaelsayed19.github.io/Template2/',
+            github: "https://github.com/YahiaElsayed19/Template2",
+            live: "https://yahiaelsayed19.github.io/Template2/",
         },
         {
-            id: 'p3',
+            id: "p3",
+            description: "Template 3 with HTML,CSS and some JS",
             image: template3,
-            description: '',
-            github: 'https://github.com/YahiaElsayed19/Template3',
-            live: 'https://yahiaelsayed19.github.io/Template3/',
+            github: "https://github.com/YahiaElsayed19/Template3",
+            live: "https://yahiaelsayed19.github.io/Template3/",
         },
         {
-            id: 'p4',
+            id: "p4",
+            description: "Complete Project with HTML,CSS and JS",
             image: completeproject,
-            description: '',
-            github: 'https://github.com/YahiaElsayed19/Complete-Project',
-            live: '',
+            github: "https://github.com/YahiaElsayed19/Complete-Project",
+            live: "https://yahiaelsayed19.github.io/Complete-Project/",
         },
         {
-            id: 'p5',
+            id: "p5",
+            description: "Quiz App with HTML,CSS and JS",
             image: quizapp,
-            description: '',
-            github: 'https://github.com/YahiaElsayed19/Quiz-App',
-            live: '',
+            github: "https://github.com/YahiaElsayed19/Quiz-App",
+            live: "https://yahiaelsayed19.github.io/Quiz-App/",
         },
         {
-            id: 'p6',
+            id: "p6",
+            description: "To-Do List App with HTML,CSS and JS",
             image: todolist,
-            description: '',
-            github: 'https://github.com/YahiaElsayed19/To-do-List',
-            live: '',
+            github: "https://github.com/YahiaElsayed19/To-do-List",
+            live: "https://yahiaelsayed19.github.io/To-do-List/",
         },
         {
-            id: 'p7',
+            id: "p7",
+            description: "Complete expanse tracker app with React.Js",
             image: expensetracker,
-            description: '',
-            github: '',
-            live: '',
+            github: "https://github.com/YahiaElsayed19/expense",
+            live: "https://expense-tracker.yahia.vercel.app/",
         },
         {
-            id: 'p8',
+            id: "p8",
+            description: "Complete food order  app with React.Js",
             image: foodapp,
-            description: '',
-            github: 'https://github.com/YahiaElsayed19/Food-order-app',
-            live: '',
+            github: "https://github.com/YahiaElsayed19/Food-order-app",
+            live: "https://food-order-app-yahia.vercel.app/",
         },
-    ]
+    ];
 
-    return (
-        <div className={classes['my-work']}>
-            <div className={classes['my-work__wrapper']}>
-                <div className={classes['my-work__box']}>
-                    <p className={classes['my-work__description']}></p>
-                    <img className={classes['my-work__image']} />
-                    <div className={classes['my-work__btn']}></div>
-                </div>
+    const content = BOXES.map((box) => (
+        <div className={classes["my-work__box"]}>
+            <p className={classes["my-work__description"]}>{box.description}</p>
+            <img src={box.image} alt={box.id} className={classes["my-work__image"]} />
+            <div className={classes["my-work__btn"]}>
+                <a href={box.github}>
+                    <BsGithub />
+                </a>
+                <a href={box.live}>
+                    <BsFillEyeFill />
+                </a>
             </div>
         </div>
-    )
-}
+    ));
 
-export default MyWork
+    return (
+        <div className={classes["my-work"]}>
+            <div className={classes["my-work__wrapper"]}>
+                {content}
+            </div>
+        </div>
+    );
+};
+
+export default MyWork;
