464b7fbf0d26121ce0f9b905e1c3d7ff1bee3499
Yahia Elsayed <yahiaelsayed19@gmail.com>
2022-10-14 20:52:32 +0200

    home

--- src/components/Home/MyWork.js
+++ src/components/Home/MyWork.js
@@ -11,65 +11,66 @@ import foodapp from "../../assets/foodapp.png";
 import { BsGithub } from "react-icons/bs";
 import { BsFillEyeFill } from "react-icons/bs";
 
+const BOXES = [
+    {
+        id: "p1",
+        description: "Template 1 with HTML&CSS",
+        image: template1,
+        github: "https://github.com/YahiaElsayed19/Template1",
+        live: "https://yahiaelsayed19.github.io/Template1/",
+    },
+    {
+        id: "p2",
+        description: "Template 2 with HTML&CSS",
+        image: template2,
+        github: "https://github.com/YahiaElsayed19/Template2",
+        live: "https://yahiaelsayed19.github.io/Template2/",
+    },
+    {
+        id: "p3",
+        description: "Template 3 with HTML,CSS and some JS",
+        image: template3,
+        github: "https://github.com/YahiaElsayed19/Template3",
+        live: "https://yahiaelsayed19.github.io/Template3/",
+    },
+    {
+        id: "p4",
+        description: "Complete Project with HTML,CSS and JS",
+        image: completeproject,
+        github: "https://github.com/YahiaElsayed19/Complete-Project",
+        live: "https://yahiaelsayed19.github.io/Complete-Project/",
+    },
+    {
+        id: "p5",
+        description: "Quiz App with HTML,CSS and JS",
+        image: quizapp,
+        github: "https://github.com/YahiaElsayed19/Quiz-App",
+        live: "https://yahiaelsayed19.github.io/Quiz-App/",
+    },
+    {
+        id: "p6",
+        description: "To-Do List App with HTML,CSS and JS",
+        image: todolist,
+        github: "https://github.com/YahiaElsayed19/To-do-List",
+        live: "https://yahiaelsayed19.github.io/To-do-List/",
+    },
+    {
+        id: "p7",
+        description: "Complete expanse tracker app with React.Js",
+        image: expensetracker,
+        github: "https://github.com/YahiaElsayed19/expense",
+        live: "https://expense-tracker.yahia.vercel.app/",
+    },
+    {
+        id: "p8",
+        description: "Complete food order  app with React.Js",
+        image: foodapp,
+        github: "https://github.com/YahiaElsayed19/Food-order-app",
+        live: "https://food-order-app-yahia.vercel.app/",
+    },
+];
+
 const MyWork = () => {
-    const BOXES = [
-        {
-            id: "p1",
-            description: "Template 1 with HTML&CSS",
-            image: template1,
-            github: "https://github.com/YahiaElsayed19/Template1",
-            live: "https://yahiaelsayed19.github.io/Template1/",
-        },
-        {
-            id: "p2",
-            description: "Template 2 with HTML&CSS",
-            image: template2,
-            github: "https://github.com/YahiaElsayed19/Template2",
-            live: "https://yahiaelsayed19.github.io/Template2/",
-        },
-        {
-            id: "p3",
-            description: "Template 3 with HTML,CSS and some JS",
-            image: template3,
-            github: "https://github.com/YahiaElsayed19/Template3",
-            live: "https://yahiaelsayed19.github.io/Template3/",
-        },
-        {
-            id: "p4",
-            description: "Complete Project with HTML,CSS and JS",
-            image: completeproject,
-            github: "https://github.com/YahiaElsayed19/Complete-Project",
-            live: "https://yahiaelsayed19.github.io/Complete-Project/",
-        },
-        {
-            id: "p5",
-            description: "Quiz App with HTML,CSS and JS",
-            image: quizapp,
-            github: "https://github.com/YahiaElsayed19/Quiz-App",
-            live: "https://yahiaelsayed19.github.io/Quiz-App/",
-        },
-        {
-            id: "p6",
-            description: "To-Do List App with HTML,CSS and JS",
-            image: todolist,
-            github: "https://github.com/YahiaElsayed19/To-do-List",
-            live: "https://yahiaelsayed19.github.io/To-do-List/",
-        },
-        {
-            id: "p7",
-            description: "Complete expanse tracker app with React.Js",
-            image: expensetracker,
-            github: "https://github.com/YahiaElsayed19/expense",
-            live: "https://expense-tracker.yahia.vercel.app/",
-        },
-        {
-            id: "p8",
-            description: "Complete food order  app with React.Js",
-            image: foodapp,
-            github: "https://github.com/YahiaElsayed19/Food-order-app",
-            live: "https://food-order-app-yahia.vercel.app/",
-        },
-    ];
 
     const content = BOXES.map((box) => (
         <div className={classes["my-work__box"]}>
