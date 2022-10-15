8448e15adb38320a13f45a158b054f61ed0dd609
Yahia Elsayed <yahiaelsayed19@gmail.com>
2022-10-15 20:39:19 +0200

    contact

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Freelance from './Freelance'
 import Info from './Info'
+import Section from './Section'
 
 const Contact = () => {
     return (
         <>
             <Freelance />
             <Info />
+            <Section />
         </>
     )
 }
