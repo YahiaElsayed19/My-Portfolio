3a1729f86d2dd79632e85bbc0e70d5146cfd2d45
Yahia Elsayed <yahiaelsayed19@gmail.com>
2022-10-19 06:28:29 +0200

    Testimonials

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Features from './Features'
 import Intro from './Intro'
+import Testimonials from './Testimonials'
 
 const About = () => {
     return (
         <>
             <Intro />
             <Features />
+            <Testimonials />
         </>
     )
 }
---
+++ src/components/About/Testimonials.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import classes from './Testimonials.module.css'
+const Testimonials = () => {
+    return (
+        <div>Testimonials</div>
+    )
+}
+
+export default Testimonials
