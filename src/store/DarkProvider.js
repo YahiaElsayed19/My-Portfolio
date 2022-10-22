780b8a231688647e0ba1871ac16e9fef0d1f46c8
Yahia Elsayed <yahiaelsayed19@gmail.com>
2022-10-23 00:08:44 +0200

    dark context

---
+++ src/store/DarkProvider.js
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import DarkContext from "./dark-context";
+
+const DarkProvider = (props) => {
+    const [dark, setDark] = useState(false);
+    const toggleDark = () => {
+        setDark(!dark);
+    };
+    const darkContext = {
+        dark: dark,
+        toggleDark: toggleDark,
+    };
+    return (
+        <DarkContext.Provider value={darkContext}>
+            {props.children}
+        </DarkContext.Provider>
+    );
+};
---
+++ src/store/dark-context.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DarkContext = React.createContext({
+    dark: null,
+    toggleDark: () => { },
+})
+export default DarkContext
