25024d93bcd89c0b120086c4153464f1f5bdacf0
Yahia Elsayed <yahiaelsayed19@gmail.com>
2023-01-27 09:49:24 +0200

    add tools change some styles in my My Work

--- src/components/Home/MyWork.js
+++ src/components/Home/MyWork.js
@@ -2,7 +2,6 @@ import React, { forwardRef, useContext } from "react";
 import { motion } from 'framer-motion'
 import darkContext from '../../store/dark-context'
 import { RiGithubFill } from "react-icons/ri";
-import { RiEyeFill } from "react-icons/ri"
 import classes from "./MyWork.module.css";
 import template1 from "../../assets/template1.png";
 import template2 from "../../assets/template2.png";
@@ -18,63 +17,72 @@ import mtdb from '../../assets/mtdb.png'
 const BOXES = [
     {
         id: "p1",
-        description: "Template 1 with HTML&CSS",
+        name: "Template 1",
+        tools: ["HTML", "CSS"],
         image: template1,
         github: "https://github.com/YahiaElsayed19/Template1",
         live: "https://yahiaelsayed19.github.io/Template1/",
     },
     {
         id: "p2",
-        description: "Template 2 with HTML&CSS",
+        name: "Template 2",
+        tools: ["HTML", "CSS"],
         image: template2,
         github: "https://github.com/YahiaElsayed19/Template2",
         live: "https://yahiaelsayed19.github.io/Template2/",
     },
     {
         id: "p3",
-        description: "Template 3 with HTML,CSS and some JS",
+        name: "Template 3",
+        tools: ["HTML", "CSS", "JS"],
         image: template3,
         github: "https://github.com/YahiaElsayed19/Template3",
         live: "https://yahiaelsayed19.github.io/Template3/",
     },
     {
         id: "p4",
-        description: "Complete Project with HTML,CSS and JS",
+        name: "Complete Project",
+        tools: ["HTML", "CSS", "JS"],
         image: completeproject,
         github: "https://github.com/YahiaElsayed19/Complete-Project",
         live: "https://yahiaelsayed19.github.io/Complete-Project/",
     },
     {
         id: "p5",
-        description: "Quiz App with HTML,CSS and JS",
+        name: "Quiz App",
+        tools: ["HTML", "CSS", "JS"],
         image: quizapp,
         github: "https://github.com/YahiaElsayed19/Quiz-App",
         live: "https://yahiaelsayed19.github.io/Quiz-App/",
     },
     {
         id: "p6",
-        description: "To-Do List App with HTML,CSS and JS",
+        name: "To-Do List App",
+        tools: ["HTML", "CSS", "JS"],
         image: todolist,
         github: "https://github.com/YahiaElsayed19/To-do-List",
         live: "https://yahiaelsayed19.github.io/To-do-List/",
     },
     {
         id: "p7",
-        description: "Complete expanse tracker app with React.Js",
+        name: "Complete expanse tracker app",
+        tools: ["React.js", "CSS"],
         image: expensetracker,
         github: "https://github.com/YahiaElsayed19/expense",
         live: "https://expense-tracker.yahia.vercel.app/",
     },
     {
         id: "p8",
-        description: "Complete food order app with React.Js",
+        name: "Complete food order app",
+        tools: ["React.js", "CSS"],
         image: foodapp,
         github: "https://github.com/YahiaElsayed19/Food-order-app",
         live: "https://food-order-app-yahia.vercel.app/",
     },
     {
         id: "p9",
-        description: "Complete Movies and Tv Shows Database with React.Js",
+        name: "Complete Movies and Tv Shows Database",
+        tools: ["React.js", "CSS", "Axios", "React router", "Framer motion"],
         image: mtdb,
         github: "https://github.com/YahiaElsayed19/MTDB",
         live: "https://mtdb.vercel.app/",
@@ -100,7 +108,6 @@ const MyWork = forwardRef((props, ref) => {
             },
         }
     }
-
     const content = BOXES.map((box) => (
         <motion.div
             variants={variants}
@@ -108,7 +115,10 @@ const MyWork = forwardRef((props, ref) => {
             whileInView="visible"
             viewport={{ once: true }}
             className={classes["my-work__box"]} key={box.id}>
-            <p className={classes["my-work__description"]}>{box.description}</p>
+            <p className={classes["my-work__name"]}>{box.name}</p>
+            <div className={classes["my-work__tools"]}>
+                {box.tools.map((tool) => <div key={Math.random()} className={classes["my-work__tool"]}>{tool}</div>)}
+            </div>
             <a href={box.live} target='_blank'>
                 <img src={box.image} alt={box.id} className={classes["my-work__image"]} />
             </a>
