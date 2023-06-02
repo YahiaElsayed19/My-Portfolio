f815ce64ea5755812f98165bf799ac1425a87b0d
Yahia Elsayed <yahiaelsayed19@gmail.com>
2023-06-02 09:03:46 +0300

    remove opacity and make the animiation quicker

--- src/components/Home/MyWork.js
+++ src/components/Home/MyWork.js
@@ -97,14 +97,12 @@ const MyWork = forwardRef((props, ref) => {
     }
     const variants = {
         hidden: {
-            opacity: 0,
             y: 100,
         },
         visible: {
-            opacity: 1,
             y: 0,
             transition: {
-                duration: 0.5
+                duration: 0.3
             },
         }
     }
