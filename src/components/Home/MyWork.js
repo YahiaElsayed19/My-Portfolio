5f4cd63f064e70de666a302ecfdd5b25ebfe67c7
Yahia Elsayed <yahiaelsayed19@gmail.com>
2023-10-05 06:03:42 +0300

    add new project optimze responisve

--- src/components/Home/MyWork.js
+++ src/components/Home/MyWork.js
@@ -3,91 +3,9 @@ import { motion } from 'framer-motion'
 import darkContext from '../../store/dark-context'
 import { RiGithubFill } from "react-icons/ri";
 import classes from "./MyWork.module.css";
-import template1 from "../../assets/template1.png";
-import template2 from "../../assets/template2.png";
-import template3 from "../../assets/template3.png";
-import completeproject from "../../assets/completeproject.png";
-import quizapp from "../../assets/quizapp.png";
-import todolist from "../../assets/todolist.png";
-import expensetracker from "../../assets/expensetracker.png";
-import foodapp from "../../assets/foodapp.png";
-import mtdb from '../../assets/mtdb.png'
+import { MyProjects } from "../../util/MyProjects";
 
 
-const BOXES = [
-    {
-        id: "p1",
-        name: "Template 1",
-        tools: ["HTML", "CSS"],
-        image: template1,
-        github: "https://github.com/YahiaElsayed19/Template1",
-        live: "https://yahiaelsayed19.github.io/Template1/",
-    },
-    {
-        id: "p2",
-        name: "Template 2",
-        tools: ["HTML", "CSS"],
-        image: template2,
-        github: "https://github.com/YahiaElsayed19/Template2",
-        live: "https://yahiaelsayed19.github.io/Template2/",
-    },
-    {
-        id: "p3",
-        name: "Template 3",
-        tools: ["HTML", "CSS", "JS"],
-        image: template3,
-        github: "https://github.com/YahiaElsayed19/Template3",
-        live: "https://yahiaelsayed19.github.io/Template3/",
-    },
-    {
-        id: "p4",
-        name: "Complete Project",
-        tools: ["HTML", "CSS", "JS"],
-        image: completeproject,
-        github: "https://github.com/YahiaElsayed19/Complete-Project",
-        live: "https://yahiaelsayed19.github.io/Complete-Project/",
-    },
-    {
-        id: "p5",
-        name: "Quiz App",
-        tools: ["HTML", "CSS", "JS"],
-        image: quizapp,
-        github: "https://github.com/YahiaElsayed19/Quiz-App",
-        live: "https://yahiaelsayed19.github.io/Quiz-App/",
-    },
-    {
-        id: "p6",
-        name: "To-Do List App",
-        tools: ["HTML", "CSS", "JS"],
-        image: todolist,
-        github: "https://github.com/YahiaElsayed19/To-do-List",
-        live: "https://yahiaelsayed19.github.io/To-do-List/",
-    },
-    {
-        id: "p8",
-        name: "Complete food order app",
-        tools: ["React.js", "CSS"],
-        image: foodapp,
-        github: "https://github.com/YahiaElsayed19/Food-order-app",
-        live: "https://food-order-app-yahia.vercel.app/",
-    },
-    {
-        id: "p9",
-        name: "Complete Movies and Tv Shows Database",
-        tools: ["React.js", "CSS", "Axios", "React router", "Framer motion"],
-        image: mtdb,
-        github: "https://github.com/YahiaElsayed19/MTDB",
-        live: "https://mtdb.vercel.app/",
-    },
-];
-// {
-//     id: "p7",
-//     name: "Complete expanse tracker app",
-//     tools: ["React.js", "CSS"],
-//     image: expensetracker,
-//     github: "https://github.com/YahiaElsayed19/expense",
-//     live: "https://expense-tracker.yahia.vercel.app/",
-// },
 
 const MyWork = forwardRef((props, ref) => {
     const darkCtx = useContext(darkContext)
@@ -108,7 +26,7 @@ const MyWork = forwardRef((props, ref) => {
             },
         }
     }
-    const content = BOXES.map((box) => (
+    const content = MyProjects.map((box) => (
         <motion.div
             variants={variants}
             initial="hidden"
---
+++ src/util/MyProjects.js
@@ -0,0 +1,93 @@
+import template1 from "../assets/template1.png"
+import template2 from "../assets/template2.png";
+import template3 from "../assets/template3.png";
+import completeproject from "../assets/completeproject.png";
+import quizapp from "../assets/quizapp.png";
+import todolist from "../assets/todolist.png";
+import foodapp from "../assets/foodapp.png";
+import mtdb from '../assets/mtdb.png'
+import fashionstore from '../assets/fashionstore.png'
+// import expensetracker from "../assets/expensetracker.png";
+
+export const MyProjects = [
+    {
+        id: "p1",
+        name: "Template 1",
+        tools: ["HTML", "CSS"],
+        image: template1,
+        github: "https://github.com/YahiaElsayed19/Template1",
+        live: "https://yahiaelsayed19.github.io/Template1/",
+    },
+    {
+        id: "p2",
+        name: "Template 2",
+        tools: ["HTML", "CSS"],
+        image: template2,
+        github: "https://github.com/YahiaElsayed19/Template2",
+        live: "https://yahiaelsayed19.github.io/Template2/",
+    },
+    {
+        id: "p3",
+        name: "Template 3",
+        tools: ["HTML", "CSS", "JS"],
+        image: template3,
+        github: "https://github.com/YahiaElsayed19/Template3",
+        live: "https://yahiaelsayed19.github.io/Template3/",
+    },
+    {
+        id: "p4",
+        name: "Complete Project",
+        tools: ["HTML", "CSS", "JS"],
+        image: completeproject,
+        github: "https://github.com/YahiaElsayed19/Complete-Project",
+        live: "https://yahiaelsayed19.github.io/Complete-Project/",
+    },
+    {
+        id: "p5",
+        name: "Quiz App",
+        tools: ["HTML", "CSS", "JS"],
+        image: quizapp,
+        github: "https://github.com/YahiaElsayed19/Quiz-App",
+        live: "https://yahiaelsayed19.github.io/Quiz-App/",
+    },
+    {
+        id: "p6",
+        name: "To-Do List App",
+        tools: ["HTML", "CSS", "JS"],
+        image: todolist,
+        github: "https://github.com/YahiaElsayed19/To-do-List",
+        live: "https://yahiaelsayed19.github.io/To-do-List/",
+    },
+    {
+        id: "p8",
+        name: "Complete food order app",
+        tools: ["React.js", "CSS"],
+        image: foodapp,
+        github: "https://github.com/YahiaElsayed19/Food-order-app",
+        live: "https://food-order-app-yahia.vercel.app/",
+    },
+    {
+        id: "p9",
+        name: "Complete Movies and Tv Shows Database",
+        tools: ["React.js", "CSS", "Axios", "React router", "Framer motion"],
+        image: mtdb,
+        github: "https://github.com/YahiaElsayed19/MTDB",
+        live: "https://mtdb.vercel.app/",
+    },
+    {
+        id: "p10",
+        name: "Fashion store",
+        tools: ["Next.js", "TypeScript","Tailwaind", "MongoDB","Axios","Framer motion"],
+        image: fashionstore,
+        github: "https://github.com/YahiaElsayed19/fashion-store",
+        live: "https://fashion-store-next.vercel.app/",
+    },
+];
+// {
+//     id: "p7",
+//     name: "Complete expanse tracker app",
+//     tools: ["React.js", "CSS"],
+//     image: expensetracker,
+//     github: "https://github.com/YahiaElsayed19/expense",
+//     live: "https://expense-tracker.yahia.vercel.app/",
+// },
