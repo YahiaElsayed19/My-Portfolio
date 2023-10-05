54e7665562f01598f0882c9d75d174126d35a13f
Yahia Elsayed <yahiaelsayed19@gmail.com>
2023-10-05 06:14:02 +0300

    update my tools

--- src/components/About/Tools.js
+++ src/components/About/Tools.js
@@ -1,67 +1,16 @@
 import React,{useContext} from 'react'
 import darkContext from '../../store/dark-context'
-import photoshop from '../../assets/photoshop 1.png'
-import figma from '../../assets/figma 1.png'
-import xd from '../../assets/xd 1.png'
-import html from '../../assets/html.png'
-import css from '../../assets/css.png'
-import bootstrap from '../../assets/bootstrap.png'
-import tailwind from '../../assets/tailwind.png'
-import js from '../../assets/js.png'
-import react from '../../assets/react.png'
-import redux from '../../assets/redux.png'
 import classes from './Tools.module.css'
+import { MyTools } from '../../util/MyTools'
 
-const tools = [
-    {
-        src: photoshop,
-        name: "Photoshop",
-    },
-    {
-        src: figma,
-        name: "Figma",
-    },
-    {
-        src: xd,
-        name: "XD",
-    },
-    {
-        src: html,
-        name: "HTML",
-    },
-    {
-        src: css,
-        name: "CSS",
-    },
-    {
-        src: bootstrap,
-        name: "Bootstrap",
-    },
-    {
-        src: tailwind,
-        name: "Tailwind",
-    },
-    {
-        src: js,
-        name: "Javascript",
-    },
-    {
-        src: react,
-        name: "React.js",
-    },
 
-    {
-        src: redux,
-        name: "React Redux",
-    },
-]
 const Tools = () => {
     const darkCtx = useContext(darkContext)
     let toolsClasses = classes.tools
     if (darkCtx.dark) {
         toolsClasses = `${classes.tools} ${classes.dark}`
     }
-    const content = tools.map(tool => <div className={classes.tool} key={tool.name}>
+    const content = MyTools.map(tool => <div className={classes.tool} key={tool.name}>
         <img src={tool.src} alt={tool.name} className={classes['tools-icon']} />
         <h6 className={classes['tools-h6']}>{tool.name}</h6>
     </div>
---
+++ src/util/MyTools.js
@@ -0,0 +1,67 @@
+import photoshop from '../assets/photoshop 1.png'
+import figma from '../assets/figma 1.png'
+import xd from '../assets/xd 1.png'
+import html from '../assets/html.png'
+import css from '../assets/css.png'
+import bootstrap from '../assets/bootstrap.png'
+import tailwind from '../assets/tailwind.png'
+import js from '../assets/js.png'
+import typescript from '../assets/typescript.svg'
+import redux from '../assets/redux.png'
+import react from '../assets/react.png'
+import next from '../assets/next-js.svg'
+import mongodb from '../assets/mongodb-icon.svg'
+export const MyTools = [
+    {
+        src: photoshop,
+        name: "Photoshop",
+    },
+    {
+        src: figma,
+        name: "Figma",
+    },
+    {
+        src: xd,
+        name: "XD",
+    },
+    {
+        src: html,
+        name: "HTML",
+    },
+    {
+        src: css,
+        name: "CSS",
+    },
+    {
+        src: bootstrap,
+        name: "Bootstrap",
+    },
+    {
+        src: tailwind,
+        name: "Tailwind",
+    },
+    {
+        src: js,
+        name: "JavaScript",
+    },
+    {
+        src: typescript,
+        name: "TypeScript",
+    },
+    {
+        src: redux,
+        name: "React Redux",
+    },
+    {
+        src: react,
+        name: "React.js",
+    },
+    {
+        src: next,
+        name: "Next.js",
+    },
+    {
+        src: mongodb,
+        name: "MongoDB",
+    },
+]
